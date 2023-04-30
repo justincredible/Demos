5086568a980a85cbf41180e568adfad138d87571
justincredible <justinjfsymonds@hotmail.com>
2023-04-30 00:40:09 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use glium::{glutin, Surface};
+use glutin::dpi::PhysicalPosition;
+use glutin::{event::{Event, WindowEvent}, event_loop::ControlFlow};
+
+fn main() {
+    let event_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new()
+        .with_resizable(false)
+        .with_title("TessellWorld")
+        .with_position(PhysicalPosition::<i32>::from((50, 50)));
+    let cb = glutin::ContextBuilder::new()
+        .with_multisampling(4)
+        .with_vsync(true);
+    let display = glium::Display::new(wb, cb, &event_loop).expect("unable to create a new display");
+
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::RedrawEventsCleared => display.gl_window().window().request_redraw(),
+            Event::RedrawRequested(_) => {
+                let mut frame = display.draw();
+                frame.clear_color(0.0, 0.0, 0.0, 1.0); // as Surface
+                frame.finish().unwrap();
+            }
+            Event::WindowEvent { event, .. } => {
+                match event {
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    _ => (),
+                }
+            },
+            _ => (),
+        }
+    });
+}
