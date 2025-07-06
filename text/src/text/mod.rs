948c86cbbe4adc4423fddf2762ee49394ffd1fe2
Justin Symonds <justinjfsymonds@hotmail.com>
2025-07-06 00:10:10 -0700

    Remove unused import

--- text/src/text/mod.rs
+++ text/src/text/mod.rs
@@ -2,7 +2,6 @@ use winit::keyboard::{KeyCode, ModifiersState};
 use std::ops::BitAnd;
 mod char_string;
 mod console;
-pub use char_string::CharString;
 pub use console::Console;
 
 const MAX_LINE: usize = 256;
